9596aa7bcfa6f45dd3a0f1238a944a6176016f76
Hugo MASSONNET <hmassonn@e1r12p12.42.fr>
2018-04-14 18:40:02 +0200

    wip

--- Model/Yify.js
+++ Model/Yify.js
@@ -1,17 +1,23 @@
 const request	=	require('request');
 
-// module.exports = class Yify
-class Yify
+module.exports = class Yify
+// class Yify
 {
-	getAll()
+	getOnePage(Pagenumber)
 	{
-		request('https://yts.ag/api/v2/list_movies.json?sort_by=rating&limit=50&page=1', function (error, response, body) {
-			console.log('error:', error); // Print the error if one occurred
-			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-			console.log('body:', body); // Print the HTML for the Google homepage.
+		request('https://yts.ag/api/v2/list_movies.json?sort_by=rating&limit=50&page='+Pagenumber, function (error, response, body) {
+			if (response && response.statusCode == 200)
+				return JSON.parse(body);
+			// console.log('error:', error); // Print the error if one occurred
+			// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+			// let obj = JSON.parse(body);
+			// // for (var i = 0; i < obj.data.movies.length; i++) {
+			// // 	obj.data.movies[i];
+			// // }
+			// console.log('body:', obj.data.movies[0].background_image) // Print the HTML for the Google homepage.
 		});
 	}
 }
 
-let y = new Yify;
-y.getAll();
+// let y = new Yify;
+// let res = y.getOnePage(1);
