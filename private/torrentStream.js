01e96d222111b5bbe46e8e56b7b79ff7fda672e6
Hugo MASSONNET <hmassonn@e2r12p12.42.fr>
2018-04-18 17:04:12 +0200

    cyprian push

--- private/torrentStream.js
+++ private/torrentStream.js
@@ -20,10 +20,53 @@
 // 	});
 // });
 
-const PirateBay = require('thepiratebay')
+// const PirateBay = require('thepiratebay')
+//
+// PirateBay.search('killing', {
+//   category: 'video'
+// })
+// .then(results => console.log(results))
+// .catch(err => console.log(err))
 
-PirateBay.search('killing', {
-  category: 'video'
-})
-.then(results => console.log(results))
-.catch(err => console.log(err))
+
+let get_url = () => {
+  return new Promise((resolve, reject) => {
+    https.get("https://yts.am/api/v2/list_movies.json?limit=1", (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        resolve(data);
+      });
+    }).on("error", (err) => {
+      reject(err.message);
+    });
+  });
+}
+
+get_url().then(result => {
+  let res = JSON.parse(result);
+
+  let uri = "magnet:?xt=urn:btih:" + res.data.movies[0].torrents[0].hash;
+
+  var engine = torrentStream(uri);
+
+  engine.on('ready', function() {
+      engine.files.forEach(function(file) {
+          console.log('filename:', file.name);
+          var stream = file.createReadStream();
+          let write = fs.createWriteStream("torrent/" + file.name);
+          let total = stream.length;
+          let progress = 0;
+
+          stream.on('data', (chunk) => {
+              progress += chunk.length;
+              console.log("readed chunk : " + Math.round(((progress / total) * 100)) + "%");
+          });
+          stream.pipe(write);
+      });
+  });
+});
