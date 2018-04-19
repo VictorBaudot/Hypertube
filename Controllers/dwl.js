ec66ce7ea97636762d74621a0d7c5d0990635311
Raphael BADIA <07fortune@gmail.com>
2018-04-19 16:09:43 +0200

    subtitles

--- Controllers/dwl.js
+++ Controllers/dwl.js
@@ -6,30 +6,12 @@ const connection = require('../private/db');
 const torrentStream = require('torrent-stream');
 const fs = require('fs');
 
-router.get('/', (req, res, next) => {
+router.get('/:imdb_id', (req, res, next) => {
 
-	const path = '/goinfre/Game.of.Thrones.S05E08.HDTV.x264-KILLERS.mp4';
+	const path = '/goinfre/' + req.params.imdb_id + '.mp4';
     const stat = fs.statSync(path);
     const fileSize = stat.size;
     const range = req.headers.range;
-	// const uri = 'magnet:?xt=urn:btih:2c7fac7a8b18716e7209fc4ad769642deab43ae1&dn=Game.of.Thrones.S05E08.HDTV.x264-KILLERS%5Bettv%5D&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969';
-	// var engine = torrentStream(uri);
-
-	// engine.on('ready', function() {
-	//     engine.files.forEach(function(file) {
-	//         console.log('filename:', file.name);
-	//         var stream = file.createReadStream();
-	//         let write = fs.createWriteStream("/goinfre/" + file.name);
-	//         let total = stream.length;
-	//         let progress = 0;
-
-	//         stream.on('data', (chunk) => {
-	//             progress += chunk.length;
-	//             console.log("readed chunk : " + Math.round(((progress / total) * 100)) + "%");
-	//         });
-	//         stream.pipe(write);
-	//     });
-  // });
   
   console.log(range)
     if (range) {
@@ -62,5 +44,20 @@ router.get('/', (req, res, next) => {
 
 
 });
+
+router.get('/sub/:imdb_id', (req, res, next) => {
+
+  const path = '/goinfre/' + req.params.imdb_id + '.vtt';
+  if (fs.existsSync(path)) {
+    const stat = fs.statSync(path);
+    res.writeHead(200, {
+      'Content-Type': 'text/vtt',
+      'Content-Length': stat.size
+    })
+    fs.createReadStream(path).pipe(res)
+  } else {
+    res.send('subtitle not found')
+  }
+})
 
 module.exports = router;
