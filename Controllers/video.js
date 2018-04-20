d9957d8941b9d995cb1e4a034bc3fe1493effae5
Raphael BADIA <07fortune@gmail.com>
2018-04-20 17:41:29 +0200

    wip

--- Controllers/video.js
+++ Controllers/video.js
@@ -9,6 +9,13 @@ const moment = require('moment');
 const torrentStream = require('torrent-stream');
 const srt2vtt = require('srt-to-vtt');
 const fs = require('fs');
+const http = require('http');
+const OS = require('opensubtitles-api');
+const OpenSubtitles = new OS({
+    useragent: 'TemporaryUserAgent',
+    username: 'rbadia',
+    password: '!^n^b%tmrfm57P2Io^s5'
+});
 
 router.get('/:id', (req, res) => {
   let coms = [], genres = [], directors = [], actors = [];
@@ -20,6 +27,7 @@ router.get('/:id', (req, res) => {
 
     video = result[0];
     dlTorrent()
+    dlSubtitle()
     display(res);
 
     // if (Object.keys(result).length > 0) {
@@ -37,45 +45,90 @@ router.get('/:id', (req, res) => {
     return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) : '';
   }
 
+  function dlSubtitle() {
+    
+  }
+
   function dlTorrent() {
 	sql.select('*', 'downloads', {}, {imdb_id: video.imdb_id})
 	.then(resp => {
 		if (resp.length == 0) {
-			sql.insert('downloads', {
+			return sql.insert('downloads', {
 				imdb_id: video.imdb_id,
 				started: 0,
-				progress: 0
+        progress: 0,
+        subtitles: 0
 			})
-		}
-	})
-	var engine = torrentStream(video.magnet);
-    engine.on('ready', function () {
-      engine.files.forEach(function (file) {
-        var stream = file.createReadStream();
-        let write = fs.createWriteStream("/goinfre/" + video.imdb_id + '.' + file.name.split('.').pop());
-        let total = stream.length;
-        let progress = 0;
-
-        stream.on('data', (chunk) => {
-          progress += chunk.length;
-		  console.log(Math.round(((progress / total) * 100)))
-          sql.update('downloads', 'imdb_id', video.imdb_id, {
-            started: 1,
-            progress: Math.round(((progress / total) * 100))
-          })
-          console.log(`readed chunk for torrent ${video.title} : `, Math.round(((progress / total) * 100)) + "%");
+    } else {
+      return Promise.resolve(resp[0].progress)
+    }
+  })
+  .then(insertResult => {
+    insertResult = 99
+    if (insertResult != 100) {
+      // torrent is new and we have to begin download it
+      var engine = torrentStream(video.magnet)
+      engine.on('ready', function () {
+        engine.files.forEach(function (file) {
+          var stream = file.createReadStream();
+          let write = fs.createWriteStream("/goinfre/" + video.imdb_id + '.' + file.name.split('.').pop());
+          let total = stream.length;
+          let progress = 0;
+  
+          stream.on('data', (chunk) => {
+            progress += chunk.length;
+            let progressPercent = Math.round(((progress / total) * 100))
+            sql.update('downloads', 'imdb_id', video.imdb_id, {
+              started: 1,
+              progress: progressPercent == 100 ? 99 : progressPercent
+            })
+            // console.log(`readed chunk for torrent ${video.title} (progress: ${progress}, total: ${total}: `, Math.round(((progress / total) * 100)) + "%");
+          });
+          stream.pipe(write);
         });
-        stream.pipe(write);
       });
-    });
 
-    engine.on('idle', function() {
-      var srtPath = `/goinfre/${video.imdb_id}.srt`;
-      if (fs.existsSync(srtPath))
-        fs.createReadStream(srtPath)
-        .pipe(srt2vtt())
-        .pipe(fs.createWriteStream(`/goinfre/${video.imdb_id}.vtt`))
-    })
+      // kan c fini
+      engine.on('idle', function() {
+        sql.update('downloads', 'imdb_id', video.imdb_id, {
+          progress: 100
+        })
+
+        OpenSubtitles.search({
+          imdbid: 'tt1431045',
+          sublanguageid: 'fre,eng'
+        })
+        .then(subtitles => {
+          console.log("wowowowowowoowowowwoowowowoowow")
+          for (let lang in subtitles) {
+            let destSrt = `/goinfre/${video.imdb_id}-${subtitles[lang].langcode}.srt`
+            console.log(`created ${destSrt}`)
+            let subFile = fs.createWriteStream(destSrt)
+            let request = http.get(subtitles[lang].url, response => {
+              response.pipe(subFile)
+            })
+            request.on('finish', () =>  {
+              if (fs.existsSync(destSrt)) {
+                console.log(`${destSrt} exists`)
+                let stream = fs.createReadStream(destSrt)
+                  .pipe(srt2vtt())
+                  .pipe(fs.createWriteStream(`/goinfre/${video.imdb_id}-${subtitles[lang].langcode}.vtt`))
+                  stream.on('finish', () => {
+                    // send dis shit to db
+                    // sql.update('downloads', 'imdb_id', video.imdb_id, {
+                    //   subtitles: 1
+                    // })
+                    process.exit(0)
+                  })
+              }
+            })
+            
+          }
+          console.log("wowowowowowoowowowwoowowowoowow")
+        })
+      })
+    }
+  })
   }
 
   function display(res) {
@@ -152,6 +205,7 @@ router.get('/:id', (req, res) => {
   }
 })
 
+// ENDPOINT TO CHECK IF THE VIDEO IS READY TO BE PLAYED
 router.get('/download/:imdb_id', (req, res) => {
   // console.log("req.params", req.params)
   sql.select('*', 'downloads', {}, {imdb_id: req.params.imdb_id})
@@ -159,7 +213,7 @@ router.get('/download/:imdb_id', (req, res) => {
     // console.log('yay', dbResult)
     if (!dbResult.length)
       res.send('error: cannot reach sql')
-    res.send(`${dbResult[0].progress}`)
+    res.send(dbResult[0])
   })
   .catch(err => {
     res.send('error: cannot reach sql')
