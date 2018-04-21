b85ed5adb96fa7958f291d752bb315316bdfb0eb
VictorBaudot <vbaudot@student.42.fr>
2018-04-21 13:21:34 +0200

    Added oauth login with linkedin and github

--- private/sql_script.js
+++ private/sql_script.js
@@ -11,14 +11,16 @@ connection.query("DROP TABLE IF EXISTS users;");
 connection.query("CREATE TABLE `users` ( \
 	`id` int(11) NOT NULL AUTO_INCREMENT, \
 	`fortytwoId` int(11) NULL, \
-	`facebookId` BIGINT NULL, \
-	`twitterId` BIGINT NULL, \
+	`facebookId` varchar(255) NULL, \
+	`githubId` varchar(255) NULL, \
+	`linkedinId` varchar(255) NULL, \
+	`twitterId` varchar(255) NULL, \
 	`email` varchar(255) DEFAULT NULL, \
 	`login` varchar(255) NOT NULL, \
 	`psswd` varchar(255) NULL, \
 	`last_name` varchar(255) DEFAULT NULL, \
 	`first_name` varchar(255) DEFAULT NULL, \
-	`photo` longtext NULL, \
+	`photo` longtext NOT NULL, \
 	`token` TEXT NULL, \
 	`email_confirmed` binary(1) NOT NULL DEFAULT '0', \
 	`lang` varchar(255) DEFAULT 'en', \
